8316121a70c9f613e842b306332b8c21b4f4c8cf
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2023-01-24 15:58:54 -0400

    avançando em node

--- src/server.js
+++ src/server.js
@@ -1,6 +1,26 @@
 import http from "node:http"
 
+const users = []
+
 const server = http.createServer((req, res)=>{
+
+    const { method, url } = req
+
+    if(method === "GET" && url === "/users"){
+        return res
+            .setHeader("Content-type", "aplication/json")
+            .end(JSON.stringify(users))
+    }
+    if(method === "POST" && url === "/users"){
+        users.push({
+            id:1,
+            name:"Ikaro Montanari",
+            email:"ikaro.montanari@exempla.com"
+        })
+
+        return res.end("Criação de usuário")
+    }
+    
     return res.end("Hello world")
 })
 
