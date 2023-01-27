8d25a3760adf77974c5b411887ccd1178901cf13
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2023-01-27 09:39:59 -0400

    iniciando com banco de dados local

---
+++ src/database.js
@@ -0,0 +1,18 @@
+export class Database {
+    #database = {}
+
+    insert(table, data){
+
+        if(Array.isArray(this.#database[table])){
+            this.#database[table].push(data)
+        }else{
+            this.#database[table] = [data]
+        }
+
+    }
+
+    select(table){
+        const data = this.#database[table] ?? []
+        return data
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import http from "node:http"
 import {  randomUUID } from "node:crypto"
 import { json } from "./middlewares/json.js"
-const users = []
+import { Database } from "./database.js"
+// const users = []
+
+const database = new Database()
 
 const server = http.createServer(async(req, res)=>{
 
@@ -10,17 +13,17 @@ const server = http.createServer(async(req, res)=>{
     await json(req, res)
 
     if(method === "GET" && url === "/users"){
-        return res
-            .setHeader("Content-type", "aplication/json")
-            .end(JSON.stringify(users))
+        const users = database.select("users")
+        return res.end(JSON.stringify(users))
     }
     if(method === "POST" && url === "/users"){
         const { name, email } = req.body
-        users.push({
+        const user ={
             id: randomUUID(),
             name,
             email
-        })
+        }
+        database.insert("users", user)
 
         return res.writeHead(201).end()
     }
